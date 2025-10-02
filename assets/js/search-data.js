5e1116768c1e6d5d69f04636282d504bd9a3934b
uofadblab <omidvart@ualberta.ca>
2025-10-02 21:38:16 +0000

    Deploying to gh-pages from @ uofadblab/uofadblab.github.io@542fb57f9a9a07748e336609ec36541078b8cd06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,15 +9,15 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-recent-projects",
-          title: "Recent Projects",
+  },{id: "nav-projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-recent-publications",
-          title: "Recent Publications",
+        },{id: "nav-publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
